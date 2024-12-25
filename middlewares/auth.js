64f44671a5e168321dd08a0fd486d598f37d0f10
Anatoly-Karpov79 <100016238+Anatoly-Karpov79@users.noreply.github.com>
2024-12-25 11:19:12 +0100

    Update auth.js 2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,14 +5,22 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 const AuthError = require('../errors/autherror');
 
 module.exports = (req, res, next) => {
-  const token = req.headers;
+  const { authorization } = req.headers;
+//  const token 
+   if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация hhhh' });
+   }
 
+  const token = authorization.replace('Bearer ', '');
+  
   let payload;
 
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    next(new AuthError('Необходимо авторизоваться token 21'));
+    next(new AuthError('Необходимо авторизоваться token 121'));
     return;
   }
 
