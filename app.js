74980f2a12e219c1ce2662b180c23319797b9781
Anatoly-Karpov79 <100016238+Anatoly-Karpov79@users.noreply.github.com>
2024-12-31 09:38:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors);
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
