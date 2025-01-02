396d5d6bf2c9178b6c8260ed77ac5397802e16dc
Anatoly-Karpov79 <100016238+Anatoly-Karpov79@users.noreply.github.com>
2025-01-02 11:03:16 +0100

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -13,11 +13,11 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-//  const requestHeaders = req.headers['access-control-request-headers'];
+ const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
+ //   res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
