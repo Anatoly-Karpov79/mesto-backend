89e8add34d4d3e3c4c5ee7560b5a13bbef002503
Anatoly-Karpov79 <100016238+Anatoly-Karpov79@users.noreply.github.com>
2025-01-02 09:28:49 +0100

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,48 +1,23 @@
-const jwt = require('jsonwebtoken');
-
-const { NODE_ENV, JWT_SECRET } = process.env;
-
 const AuthError = require('../errors/autherror');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const token = req.headers;
+  const { authorization } = req.headers;
+//  const token 
+   if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация hhhh' });
+   }
 
+  const token = authorization.replace('Bearer ', '');
+  
   let payload;
 
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    next(new AuthError('Необходимо авторизоваться token'));
+    next(new AuthError('Необходимо авторизоваться token 21'));
+    next(new AuthError('Необходимо авторизоваться token 121'));
     return;
   }
-
-  req.user = payload;
-
-  next();
-};
-
-//module.exports = (req, res, next) => {
-//  const { authorization } = req.headers;
-//  const token 
-//   if (!authorization || !authorization.startsWith('Bearer ')) {
-//    return res
- //     .status(401)
-//      .send({ message: 'Необходима авторизация hhhh' });
- //  }
-
-//  const token = authorization.replace('Bearer ', '');
-  
-//  let payload;
-
-//  try {
-//    payload = jwt.verify(token, 'some-secret-key');
-//  } catch (err) {
-//    next(new AuthError('Необходимо авторизоваться token 121'));
-//    return;
-//  }
-
-  req.user = payload;
-
-  next();
-};
-// авторизация на локале
