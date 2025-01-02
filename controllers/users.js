2767012f21acd1b1c90ca01027bca8a0c2ed2892
Anatoly-Karpov79 <100016238+Anatoly-Karpov79@users.noreply.github.com>
2025-01-02 09:36:13 +0100

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -120,13 +120,9 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id },'some-secret-key', { expiresIn: '7d' });
-      res.status(STATUS_OK).cookie('jwt', token, {
-        maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
-        sameSite: 'None',
-        secure: true,
-      }).send(user.toJSON());
+      const token = jwt.sign({ _id: user._id }, 'some-secret-key');
+      res.send({ token }, user.toJSON());
+    
     })
     .catch(next);
 };
